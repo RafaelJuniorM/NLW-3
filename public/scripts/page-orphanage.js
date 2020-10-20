7409d8f1157298f21c5bf5182c90fa974cc70a9e
RafaelJuniorM <rafael.junior15@hotmail.com>
2020-10-20 07:38:34 -0300

    page create orphanage

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -28,3 +28,23 @@ L
 .marker([-19.9240083,-43.9410837], {icon})
 .addTo(map)
 
+/* image gallery*/
+
+function selectImage(event){
+  const button = event.currentTarget
+  
+  // remover todas as classes .active
+  const buttons = document.querySelectorAll(".images button")
+  buttons.forEach(removeActiveClass)
+
+  function removeActiveClass(button){
+    button.classList.remove("active")
+  }
+  // selecioanr a imagem clicada
+  const image = button.children[0]
+  const imageContainer = document.querySelector(".orphanage-details > img")
+  //atualizar o container de image, 
+  imageContainer.src = image.src
+  // adicionar a classe .active para este botao que foi clicado
+  button.classList.add('active')
+}
